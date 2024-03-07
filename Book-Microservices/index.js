a3f531456c75963441ab56050ea4275789f4f87c
sethu <callmesethu@gmail.com>
2024-03-07 08:30:07 +0530

    connecting to mongoDB

---
+++ Book-Microservices/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const customResourceResponse = require("./utils/constant");
+const dbConfig = require("./config/databaseconfig");
+const bookRoutes = require("./route/bookRoute");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(dbConfig.booksMongoURI, {})
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/v1", bookRoutes);
+
+//404 handler
+app.use((req, res) => {
+  res.status(404).send({
+    message: "The requested URL could not be found.",
+    statusCode: 404,
+  });
+});
+
+app.use((error, req, res, next) => {
+  const { message } = customResourceResponse.serverError;
+  const data = {
+    Code: `${error.code ? error.code : ""}`,
+    Stacktrace: `${error.stack}`,
+  };
+  res.status(500).json({ message, data });
+});
+
+const port = 8000;
+
+app.listen(port, () =>
+  console.log("Book server started and listening on port" + port)
+);
